44fe58ea4c9de7dc9205cf57d76d22906de9300e
MS2329 <MS2329@CILABS-119B-10.scilsnet.rutgers.edu>
2017-10-27 19:09:53 -0400

    Apple Picker pt. 6
    Added code to add High Scpre and to preseve player's high score

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -36,6 +36,11 @@ public class ApplePicker : MonoBehaviour {
         GameObject tBasketGO = basketList[basketIndex];
         basketList.RemoveAt(basketIndex);
         Destroy(tBasketGO);
+
+        if (basketList.Count == 0)
+        {
+            SceneManager.LoadScene("_Scene_0");
+        }
     }
 	// Update is called once per frame
 	void Update ()
